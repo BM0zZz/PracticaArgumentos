3f4b2fa9e8ef6275bc3b7222c0cc6df6235accb6
victor_lgns11 <victornievezzz@gmail.com>
2025-10-01 12:20:31 +0200

    Update LanzarNievesBrionesVictorEj1.java

--- LanzarNievesBrionesVictorEj1.java
+++ LanzarNievesBrionesVictorEj1.java
@@ -1,9 +1,14 @@
 package lvchi;
 
+import java.io.File;
+
 public class LanzarNievesBrionesVictorEj1 {
     public static void main(String[] args) {
+        File ficheroEntrada = new File(args[0]);
+        File ficheroSalida = new File(args[1]);
+        String laClase = "Actividad1Bytes.Nieves_Briones_Victor_AD_Ej1";
         try {
-            ProcessBuilder pb = new ProcessBuilder("java", "lvchi.Nieves_Briones_Victor_AD_Ej1", "entrada.txt", "salida.txt");
+            ProcessBuilder pb = new ProcessBuilder("java", laClase, ficheroEntrada.getPath(), ficheroSalida.getPath());
             Process proceso = pb.start();
             int codigo = proceso.waitFor();
             System.out.println("Código de salida: " + codigo);
